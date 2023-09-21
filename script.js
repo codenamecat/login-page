4d399cd2cd9e3194f60161388e53e3dc7cb28b64
codenamecat <113186187+codenamecat@users.noreply.github.com>
2023-09-21 14:56:53 +0300

    accessibility overhaul, animations fixed

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
-const menuBtn = document.getElementById('menu-icon')
-const searchBtn = document.getElementById('search-icon')
+const menuBtn = document.getElementById('menu-button')
+const searchBtn = document.getElementById('search-button')
 const dropdownMenu = document.getElementById('dropdown-menu')
 const searchBar = document.getElementById('search-bar')
 const loginBtn = document.getElementById('login-button')
 const signUpBtn = document.getElementById('signup-button')
 const emailInput = document.getElementById('email')
 const passwordInput = document.getElementById('password')
+const menuIcon = document.getElementById('menu-icon')
+const searchIcon = document.getElementById('search-icon')
 
 menuBtn.addEventListener('click', () => {
   dropdownMenu.classList.toggle('visible')
-  menuBtn.classList.toggle('open-menu')
+  menuIcon.classList.toggle('open-menu')
   searchBar.classList.remove('visible')
-  searchBtn.classList.remove('open-search')
+  searchIcon.classList.remove('open-search')
 })
 
 searchBtn.addEventListener('click', () => {
   searchBar.classList.toggle('visible')
-  searchBtn.classList.toggle('open-search')
+  searchIcon.classList.toggle('open-search')
   dropdownMenu.classList.remove('visible')
-  menuBtn.classList.remove('open-menu')
+  menuIcon.classList.remove('open-menu')
 })
 
 loginBtn.addEventListener('click', (event) => {

73fe33fc09e0f68f0d5260577a6238461ee77817
codenamecat <113186187+codenamecat@users.noreply.github.com>
2023-09-21 13:12:28 +0300

    animations added

--- script.js
+++ script.js
@@ -11,11 +11,14 @@ menuBtn.addEventListener('click', () => {
   dropdownMenu.classList.toggle('visible')
   menuBtn.classList.toggle('open-menu')
   searchBar.classList.remove('visible')
+  searchBtn.classList.remove('open-search')
 })
 
 searchBtn.addEventListener('click', () => {
   searchBar.classList.toggle('visible')
+  searchBtn.classList.toggle('open-search')
   dropdownMenu.classList.remove('visible')
+  menuBtn.classList.remove('open-menu')
 })
 
 loginBtn.addEventListener('click', (event) => {
